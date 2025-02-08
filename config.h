9f0cf3f8f4f22eaad6c360b3967aa86fce490669
gczcn <494353540@qq.com>
2025-02-08 22:26:33 +0800

    update

--- config.h
+++ config.h
@@ -99,24 +99,24 @@ float alpha = 1.0;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	[0] = "#2d353b", /* black & background */
-	[1] = "#e67e80", /* red     */
-	[2] = "#a7c080", /* green   */
-	[3] = "#dbbc7f", /* yellow  */
-	[4] = "#7fbbb3", /* blue    */
-	[5] = "#d699b6", /* magenta */
-	[6] = "#83c092", /* cyan    */
-	[7] = "#d3c6aa", /* white   */
+	[0] = "#282828", /* black & background */
+	[1] = "#ea6962", /* red     */
+	[2] = "#a9b665", /* green   */
+	[3] = "#d8a657", /* yellow  */
+	[4] = "#7daea3", /* blue    */
+	[5] = "#d3869b", /* magenta */
+	[6] = "#89b482", /* cyan    */
+	[7] = "#d4be98", /* white   */
 
 	/* 8 bright colors */
-	[8]  = "#475258", /* black   */
-	[9]  = "#e67e80", /* red     */
-	[10] = "#a7c080", /* green   */
-	[11] = "#dbbc7f", /* yellow  */
-	[12] = "#7fbbb3", /* blue    */
-	[13] = "#d699b6", /* magenta */
-	[14] = "#83c092", /* cyan    */
-	[15] = "#d3c6aa", /* white   */
+	[8]  = "#3c3836", /* black   */
+	[9]  = "#ea6962", /* red     */
+	[10] = "#a9b665", /* green   */
+	[11] = "#d8a657", /* yellow  */
+	[12] = "#7daea3", /* blue    */
+	[13] = "#d3869b", /* magenta */
+	[14] = "#89b482", /* cyan    */
+	[15] = "#d4be98", /* white   */
 };
 
 
